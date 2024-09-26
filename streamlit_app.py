233031cd3d0099838d07dabaf4a1fd8b4274cacf
shua10 <83661271+shua10@users.noreply.github.com>
2024-09-25 20:36:23 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,10 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+df = pd.DataFrame({
+    'Fecha': ['2024-05-01', '2024-05-02', '2024-05-03', '2024-05-04'],
+    'Datos': [1, 2, 3, 4]
+})
+
+st.line_chart(df)
